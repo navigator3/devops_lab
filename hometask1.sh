e6bbb201739bf219259c9ebc45070ad4e8b2a3e0
sshevtsov <sergei_shevtsov@epam.com>
2020-09-18 22:26:07 +0300

    homework_1 python

---
+++ hometask1.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+##################################################
+#
+#   Sergei Shevtsov homeTask1,Python
+#
+##################################################
+sudo pyenv install 2.7.5
+sudo pyenv install 3.7.9
+sudo pyenv virtualenv virt1 2.7.5
+sudo pyenv virtualenv virt2 3.7.9
