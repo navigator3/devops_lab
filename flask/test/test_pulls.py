ef4209b4349484f86353f428ce11e569651a094b
sshevtsov <sergei_shevtsov@epam.com>
2020-09-29 00:05:11 +0300

    relocate test files

--- flask/test/test_pulls.py
+++ flask/test/test_pulls.py
@@ -1,7 +1,6 @@
 from unittest import TestCase
-import handlers.pulls as pulls
 import setts
-
+import handlers.pulls
 print(setts.json)
 
 
@@ -11,21 +10,21 @@ class TestPrime(TestCase):
         """Init"""
     def test_connect(self):
         """Test for is _connect"""
-        self.assertIsInstance(pulls.connect(1, 2, 3), list)
+        self.assertIsInstance(handlers.pulls.connect(1, 2, 3), list)
 
     def test_get_pulls(self):
         """Test for is _get_pulls"""
-        self.assertIsInstance(pulls.get_pulls(1, 2, 3), list)
+        self.assertIsInstance(handlers.pulls.get_pulls(1, 2, 3), list)
 
     def test_process(self):
         """Test for is _prime"""
-        self.assertEqual(pulls.process(setts.json, "open"),
+        self.assertEqual(handlers.pulls.process(setts.json, "open"),
                          setts.json_open)
-        self.assertEqual(pulls.process(setts.json, "accepted"),
+        self.assertEqual(handlers.pulls.process(setts.json, "accepted"),
                          setts.json_accepted)
-        self.assertEqual(pulls.process(setts.json, "needs work"),
+        self.assertEqual(handlers.pulls.process(setts.json, "needs work"),
                          setts.json_needs_work)
-        self.assertEqual(pulls.process(setts.json, "closed"),
+        self.assertEqual(handlers.pulls.process(setts.json, "closed"),
                          setts.json_closed)
 
     def tearDown(self):
